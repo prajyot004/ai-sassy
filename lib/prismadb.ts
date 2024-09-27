f1f0a20f57a1c38bd779c092b549ae48b52ddb05
Naveed <mastermindgeh@gmail.com>
2024-09-28 01:21:43 +0500

    prisma not defined error resolved

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,14 +1,15 @@
 // lib/prisma.js
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
+let prisma;
 
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-    global.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
